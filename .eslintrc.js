afdc7b0100f5b01e9fc7db05bf8619563c06de38
Ben Chauvette <bdchauvette@gmail.com>
2016-05-07 15:29:16 -0400

    Update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 const config = {
   extends: 'airbnb',
+  rules: {
+    'no-use-before-define': ['error', { functions: false }],
+    'quote-props': ['error', 'consistent-as-needed'],
+  },
 };
 
 module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const isProduction = (NODE_ENV === 'production');
 
 const config = {
-  entry: [
-    path.join(__dirname, 'src/index.js'),
-  ],
+  entry: {
+    app: path.join(__dirname, 'src/index.js'),
+    vendor: [
+      'baobab',
+      'baobab-react',
+      'isogram',
+      'react',
+      'react-dom',
+      'react-highlight',
+    ],
+  },
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'app.js',
     publicPath: 'build',
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-  ],
   module: {
     loaders: [
       {
@@ -26,11 +32,16 @@ const config = {
       },
       {
         test: /\.css?$/,
-        loaders: ['style', 'raw'],
+        loader: ExtractTextPlugin.extract('style-loader', 'raw-loader'),
         include: __dirname,
       },
     ],
   },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new ExtractTextPlugin('main.css', { allChunks: true }),
+  ],
 };
 
 if (!isProduction) {
