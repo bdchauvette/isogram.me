c37ab0b20b2c580279a47eddff8928c3993e62b1
Ben Chauvette <bdchauvette@gmail.com>
2016-05-13 00:12:57 -0400

    Fix up our own GA code

--- src/index.js
+++ src/index.js
@@ -14,22 +14,31 @@ import stateTree from './stateTree';
 
 // Output our own cheeky Google Analytics code
 const isograms = [
-  /* aloha          */ ['A', 'l', 'o', 'h', 'a'],
-  /* annyeonghaseyo */ ['안', '녕', '하', '세', '요'],
-  /* ciao           */ ['c', 'i', 'a', 'o'],
-  /* hallo          */ ['h', 'a', 'l', 'L', 'o'],
-  /* hello          */ ['h', 'e', 'l', 'L', 'o'],
-  /* hey            */ ['h', 'e', 'y'],
-  /* hola           */ ['h', 'o', 'l', 'a'],
-  /* howdy          */ ['h', 'o', 'w', 'd', 'y'],
-  /* konnichiwa     */ ['こ', 'ん', 'に', 'ち', 'は'],
-  /* muli bwa       */ ['m', 'u', 'l', 'i', 'b', 'w', 'a'],
-  /* salut          */ ['s', 'a', 'l', 'u', 't'],
-  /* welcome        */ ['w', 'e', 'l', 'c', 'o', 'm', 'E'],
-  /* whatsup        */ ['w', 'h', 'a', 't', 's', 'u', 'p'],
+  '$_π',
+  'Aloha',
+  'atihow',
+  '안녕하세요',
+  'ciao',
+  'HaLlO',
+  'HeLlO',
+  'hey',
+  'hola',
+  'howdy',
+  'IsOgRaM',
+  'ISOGRAM',
+  'こんにちは',
+  'margosi',
+  'MaRgOsI',
+  'mulibwa',
+  'salut',
+  'trackme',
+  'WelcomE',
+  'whatsup',
 ];
 const randomIsogram = Math.floor(Math.random() * isograms.length);
-const isogramScript = isogram(isograms[randomIsogram].join(''));
+const isogramScript = isogram(isograms[randomIsogram], {
+  id: 'UA-77741908-1',
+});
 document.write(`<script>${isogramScript}</script>`);
 
 // Render the application
